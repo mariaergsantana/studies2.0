59b10e4a923264b8fd2fdecd0e940df75d3e7e0f
Maria Gomes <mariaergsantana@outlook.com>
2025-08-06 10:24:01 -0300

    JS
    This is my redo to practice the lessons learned on the freeCodeCamp

---
+++ freeCodeCamp/variables_and_strings/main.js
@@ -0,0 +1,15 @@
+// This project is to build a Greeting Robot, following the step-by-step from the freeCodeCamp lectures
+
+// console.log --> showcases the message
+console.log("Hi, there!");
+console.log("I am excited to talk to you.");
+
+// let --> create variables
+let bot;
+let botLocation;
+
+// the following instruction sets a value to the variables
+bot = "teacherBot";
+botLocation = "the universe";
+
+console.log("Allow me to introduce myself.")
